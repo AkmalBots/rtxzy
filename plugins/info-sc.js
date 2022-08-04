e7ac4a2d65bef11b591e146533abc2ea82e99245
AlyaaXzy_72 <96462315+AlyaaXd@users.noreply.github.com>
2022-08-04 23:30:04 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,8 +1,16 @@
+/*
+ * ngelarang lu biar g diubah kyknya emg susah ya anjg
+ * jadi kalau lu g bsa ngehargain orang
+ * setidaknya buat sc ori by gw, trs buat sc lu hasil copas tuh
+ * better bgtu dripada nma gw dihapus smua
+ * najis 👎🗿
+*/
+
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Bot Ini Tidak open source :)\n\n Tapi kalian bisa menggunakan Database Bot Ini\n\n> https://database.tioclkp02.repl.co\n> https://json-server.tioclkp02.repl.co
-`
+Hai ${ye} Lagi Nyari Sc Ya? 
+Nih Link Sc Nya Dibawah :-`
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
 handler.help = ['sc', 'sourcecode']
